e39332f0c4325c5238d1b2d38e718a2d2ad3a238
Devashish Pandey <devashishpandey33@gmail.com>
2024-12-18 14:05:19 +0530

    committed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ root.render(
   <React.StrictMode>
       <BrowserRouter> 
       <CssVarsProvider>
+
           <App />
+          
         </CssVarsProvider>
       </BrowserRouter>
   </React.StrictMode>
