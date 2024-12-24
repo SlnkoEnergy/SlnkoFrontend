5b9482e1ca5cf8ecad430a24c4d0e5dde251a740
Devashish Pandey <devashishpandey33@gmail.com>
2024-12-24 17:29:01 +0530

    code committed

--- src/pages/Users/auth/Login.js
+++ src/pages/Users/auth/Login.js
@@ -78,7 +78,7 @@ const Login = () => {
     };
     try {
       const response = await axios.post(
-        "https:147.93.20.206:8080/v1/login",
+        "http:147.93.20.206:8080/v1/login",
         postData
       );
       console.log("Login successful:", response.data);
