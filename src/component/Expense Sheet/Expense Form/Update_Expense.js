73d37927ba306a72b61f5a6124c06453ccb47874
KumarRounik <89687049+KumarRounik@users.noreply.github.com>
2025-05-29 10:30:08 +0530

    Expense

--- src/component/Expense Sheet/Expense Form/Update_Expense.js
+++ src/component/Expense Sheet/Expense Form/Update_Expense.js
@@ -657,7 +657,7 @@ const UpdateExpense = () => {
             alignItems="center"
             gap={2}
           >
-            {/* Left: Date Range */}
+            {/* Expense Term (from-to) */}
             <Box display="flex" alignItems="center" gap={2} flexWrap="wrap">
               <Typography level="body-md" fontWeight="lg">
                 Select Expense Term:
