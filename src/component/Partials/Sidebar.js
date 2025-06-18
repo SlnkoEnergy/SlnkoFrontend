774de0cd4a3701d13e487bdc51b9f9a35231d10b
Devashish Pandey <devashishpandey33@gmail.com>
2025-06-18 18:10:03 +0530

    deepak sir access done

--- src/component/Partials/Sidebar.js
+++ src/component/Partials/Sidebar.js
@@ -756,7 +756,7 @@ function Sidebar() {
                       User Dashboard
                     </ListItemButton>
                   </ListItem>
-                  {user?.emp_id === "SE-099" && (
+                  {user?.department === "BD" && user?.emp_id === "SE-277" && (
                     <ListItem>
                       <ListItemButton
                         onClick={() => navigate("/expense_approval")}
