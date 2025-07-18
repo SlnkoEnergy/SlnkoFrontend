3ac74d02b8ef4740f19c449a37109ade7eca5427
KumarRounik <rounikpandey@gmail.com>
2025-07-18 10:45:26 +0530

    given access to Eng

--- src/component/Partials/Sidebar.js
+++ src/component/Partials/Sidebar.js
@@ -2508,6 +2508,7 @@ function Sidebar() {
           (user?.department === "Engineering" &&
             user?.name !== "Rishav Mahato" && 
             user?.name !=="Piyush Rathour Raj" &&
+            user?.name !=="Md Asif" &&
             user?.name !=="Rahul Bhati" &&
             user?.name !=="Sachin Jangid" &&
             user?.name !=="Vishal Kushwaha" &&
@@ -2605,6 +2606,7 @@ function Sidebar() {
         ) : user?.department === "Engineering" &&
           (user?.name === "Rishav Mahato" || 
           user?.name !=="Piyush Rathour Raj" ||
+          user?.name !=="Md Asif"||
             user?.name !=="Rahul Bhati" ||
             user?.name !=="Sachin Jangid" ||
             user?.name !=="Vishal Kushwaha" ||
