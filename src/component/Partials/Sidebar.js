32c5cb85d268fc73ba6e0107e2f9ba1cba2ecaf1
KumarRounik <rounikpandey@gmail.com>
2025-09-12 12:06:17 +0530

    given task access to hr

--- src/component/Partials/Sidebar.js
+++ src/component/Partials/Sidebar.js
@@ -2430,6 +2430,14 @@ function Sidebar() {
                 </List>
               </Toggler>
             </ListItem>
+            <ListItem sx={{ mt: 0.5 }}>
+              <ListItemButton onClick={() => navigate("/task_dashboard")}>
+                <TaskIcon />
+                <ListItemContent>
+                  <Typography level="title-sm">Task</Typography>
+                </ListItemContent>
+              </ListItemButton>
+            </ListItem>
           </List>
         ) : user?.department === "Projects" ||
           user?.department === "Infra" ||
