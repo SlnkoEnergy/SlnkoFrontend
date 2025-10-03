02e4eac09268649e54393491ec419a8408e6cb4b
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-10-03 12:18:11 +0530

    added code for resources

--- src/pages/CAM/DashboardCam.jsx
+++ src/pages/CAM/DashboardCam.jsx
@@ -1,20 +1,17 @@
 import Box from "@mui/joy/Box";
 import CssBaseline from "@mui/joy/CssBaseline";
 import { CssVarsProvider } from "@mui/joy/styles";
-import  { useEffect, useState } from "react";
-import Breadcrumbs from "@mui/joy/Breadcrumbs";
-import Link from "@mui/joy/Link";
-import Typography from "@mui/joy/Typography";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import { useEffect, useState } from "react";
 import Sidebar from "../../component/Partials/Sidebar";
-
-import Header from "../../component/Partials/Header";
-import { useNavigate } from "react-router-dom";
 import Dash_cam from "../../component/CamDashboard";
+import MainHeader from "../../component/Partials/MainHeader";
+import { Button } from "@mui/joy";
+import { useNavigate } from "react-router-dom";
+import SubHeader from "../../component/Partials/SubHeader";
 
 function Dashboard() {
   const [user, setUser] = useState(null);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const userData = getUserData();
     setUser(userData);
@@ -31,8 +28,51 @@ function Dashboard() {
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
       <Box sx={{ display: "flex", minHeight: "100dvh" }}>
-        <Header />
         <Sidebar />
+        <MainHeader title="CAM" sticky>
+          <Box display="flex" gap={1}>
+            <Button
+              size="sm"
+              onClick={() => navigate(`/cam_dash`)}
+              sx={{
+                color: "white",
+                bgcolor: "transparent",
+                fontWeight: 500,
+                fontSize: "1rem",
+                letterSpacing: 0.5,
+                borderRadius: "6px",
+                px: 1.5,
+                py: 0.5,
+                "&:hover": {
+                  bgcolor: "rgba(255,255,255,0.15)",
+                },
+              }}
+            >
+              Handover
+            </Button>
+
+            <Button
+              size="sm"
+              onClick={() => navigate(`/purchase_request`)}
+              sx={{
+                color: "white",
+                bgcolor: "transparent",
+                fontWeight: 500,
+                fontSize: "1rem",
+                letterSpacing: 0.5,
+                borderRadius: "6px",
+                px: 1.5,
+                py: 0.5,
+                "&:hover": {
+                  bgcolor: "rgba(255,255,255,0.15)",
+                },
+              }}
+            >
+              Purchase Request
+            </Button>
+          </Box>
+        </MainHeader>
+        <SubHeader title="Handover" isBackEnabled={false} sticky></SubHeader>
         <Box
           component="main"
           className="MainContent"
@@ -52,50 +92,6 @@ function Dashboard() {
             gap: 1,
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginLeft: { xl: "15%", lg: "18%" },
-            }}
-          >
-            <Breadcrumbs
-              size="sm"
-              aria-label="breadcrumbs"
-              separator={<ChevronRightRoundedIcon fontSize="sm" />}
-              sx={{ pl: 0, marginTop: { md: "4%", lg: "0%" } }}
-            >
-              <Link
-                underline="none"
-                color="neutral"
-                sx={{ fontSize: 12, fontWeight: 500 }}
-              >
-                CAM
-              </Link>
-              <Typography
-                color="primary"
-                sx={{ fontWeight: 500, fontSize: 12 }}
-              >
-                CAM Dashboard
-              </Typography>
-            </Breadcrumbs>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              mb: 1,
-              gap: 1,
-              flexDirection: { xs: "column", sm: "row" },
-              alignItems: { xs: "start", sm: "center" },
-              flexWrap: "wrap",
-              justifyContent: "space-between",
-              marginLeft: { xl: "15%", lg: "18%" },
-            }}
-          >
-            <Typography level="h2" component="h1">
-              CAM Dashboard
-            </Typography>
-          </Box>
           <Dash_cam />
         </Box>
       </Box>
