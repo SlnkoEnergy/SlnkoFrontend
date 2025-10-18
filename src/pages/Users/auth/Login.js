2c6134e285274d8c2dbc0a2049fa896ec1819896
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-10-18 10:59:22 +0530

    added diwali theme in protrac

--- src/pages/Users/auth/Login.js
+++ src/pages/Users/auth/Login.js
@@ -12,7 +12,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import * as Yup from "yup";
 import Img1 from "../../../assets/New_Solar3.png";
-import Img5 from "../../../assets/karwa_chautt_logo.png";
+import Img5 from "../../../assets/protrac_diwali.png";
 import ImgX from "../../../assets/slnko_white_logo.png";
 import axios from "axios";
 import { useAddLoginsMutation } from "../../../redux/loginSlice";
